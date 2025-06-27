9475ff4d1f19bd5a2a4f5368d79053da29b45932
ashunegi007 <negia3092@gmail.com>
2025-06-27 22:54:57 +0530

    initial commit

---
+++ start.sh
@@ -0,0 +1,16 @@
+#!/user/bin/env bash
+echo "Running composer"
+
+composer install --no-dev --working-dir=/var/www/html
+
+echo "Caching config..."
+php artisan config:cache 
+
+echo "Caching routes..."
+php artisan routes:cache 
+
+echo "Publishing cloudinary provider..."
+php artisan vendor:publish --provider="CloudinaryLabs\CloudinaryLaravel\CloudinaryServiceProvider" --tag="cloudinary-laravel-config"
+
+echo "Running migration..."
+php artisan migrate --force
